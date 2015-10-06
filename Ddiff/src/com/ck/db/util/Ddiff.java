b58510aaff01caf516ab9a678adfe621ecfa8964
s4553711 <s4553711@gmail.com>
2015-10-06 11:00:20 +0800

    Add FK diff support

--- Ddiff/src/com/ck/db/util/Ddiff.java
+++ Ddiff/src/com/ck/db/util/Ddiff.java
@@ -21,9 +21,7 @@ public class Ddiff {
     	boolean result = false;
     	List<DBSchema> schemaOfdb1 = getSchemas(this.db1);
     	List<DBSchema> schemaOfdb2 = getSchemas(this.db2);
-    	
-//    	System.out.println("1> "+schemaOfdb1.size());
-//    	System.out.println("2> "+schemaOfdb2.size());
+
     	Collection<DBSchema> diffDB = SetOperator.diffCompared(schemaOfdb1, schemaOfdb2);
     	for(DBSchema db : diffDB) {
     		System.out.println("Diff in table > name: "+db.getName());
@@ -37,6 +35,9 @@ public class Ddiff {
     			System.out.println("Diff in Col > table: "+db.getName()+", column name: "+col.getName()+" , column type: "+col.getType());
     			result = true;
     		}
+    		for ( String fk : SetOperator.diffCompared(schemaOfdb1.get(index1).getFKList(), schemaOfdb2.get(index2).getFKList())) {
+    			System.out.println("Diff in FK > table: "+db.getName()+", FK name: "+fk);
+    		}
     	}
     	return result;
     }
@@ -46,21 +47,21 @@ public class Ddiff {
         try {
         	DBSchema schema = null;
         	boolean canRead = true;
+        	boolean canReadFK = false;
         	for(String line : Files.readAllLines(new File(input).toPath(), Charset.defaultCharset())) {
         		line = line.trim();
-        		//System.out.println("process > "+line);
         		if (line.startsWith("Table")) {
         			if (schema != null) {
         				all.add(schema);
         			}
         			line = line.replace("Table \"public.", "").replace("\"", "");
         			schema = new DBSchema(line);
-        			//System.out.println("start "+line);
         			canRead = true;
+        			canReadFK = false;
         			continue;
-        		} else if (line.startsWith("Sequence") || line.startsWith("Index")) {
-        			//System.out.println("ignore "+line);
+        		} else if (line.startsWith("Sequence") || line.startsWith("Index") || line.trim().startsWith("Referenced by:")) {
         			canRead = false;
+        			canReadFK = false;
         			continue;
         		} else if(canRead) {
         			String[] cols = line.split("\\|");
@@ -71,6 +72,11 @@ public class Ddiff {
         					schema.addCol(cols[0].trim(), cols[1].trim(), "");
         				}
         			}
+        		} else if(canReadFK && !line.trim().equals("")) {
+        			schema.addFK(line.trim());
+        		} else if (line.startsWith("Foreign-key constraints:")) {
+        			canReadFK = true;
+        			continue;
         		}
         	}
         	
